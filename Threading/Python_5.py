623763455ee93bfe48f6905f80b6cdc709f225e2
R.A.Korolev <Orodunaar@mail.ru>
2024-07-01 21:02:56 +0300

    Основы асинхронности Asyncio, async/await test speed 2

---
+++ Threading/Python_5.py
@@ -0,0 +1,20 @@
+import time
+import threading
+
+
+def test():
+    while True:
+        print("test")
+        time.sleep(1)
+
+
+thr = threading.Timer(5, test)
+thr.setDaemon(True)
+thr.start()
+
+for _ in range(6):
+    print("111")
+    time.sleep(1)
+
+thr.cancel()
+print('finish')
